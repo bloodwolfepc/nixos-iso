f5315161435e5c43f215ef9f6b17454db29d8504
bloodwolfepc <bloodwolfepc@gmail.com>
2024-07-01 01:18:22 -0500

    commit

---
+++ configuration.nix
@@ -0,0 +1,16 @@
+{ pkgs, modulesPath, inputs, ... }: { 
+  imports = [
+    "${modulesPath}/installer/cd-dvd/installation-cd-minimal.nix"
+  ];
+  nixpkgs.hostPlatform = "x86_64-linux";
+  nixpkgs.config.allowUnfree = true;
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+  
+  environment.systemPackages = with pkgs; [
+    inputs.neovim.packages.x86_64-linux.default
+    git
+  ];
+  environment.sessionVariables = {
+    EDIOTR = "nvim";
+  };
+}
---
+++ flake.nix
@@ -0,0 +1,44 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/24.05";
+    neovim = {
+      url = "github:bloodwolfepc/dead";
+    };
+    nur = {
+      url = "github:nix-community/NUR";
+    };
+  };
+  outputs = { self, nixpkgs, nur, ... }@inputs: {
+    nixosConfigurations = {
+      default = nixpkgs.lib.nixosSystem {
+        specialArgs = { inherit inputs; };
+        modules = [ 
+          ./configuration.nix
+        ];
+      };
+      nixos-iso = nixpkgs.lib.nixosSystem {
+        specialArgs = { inherit inputs; };
+        modules = [ 
+          ./configuration.nix
+          nur.nixosModules.nur
+          ({ config, ... }: {
+            environment.systemPackages = [ config.nur.repos.johnazoidberg.ssacli ];
+          })
+          
+          #This imports modules 
+           
+          #{ nixpkgs.overlays = [ nur.overlay ]; } #overlay for ssacli
+          #({ pkgs, ... }:
+          #  let
+          #    nur-no-pkgs = import nur {
+          #      nurpkgs = import nixpkgs { system = "x86_64-linux"; };
+          #    };
+          #  in {
+          #    environment.systemPackages = [ nur-no-pkgs.repos.johnazoidberg.ssacli ];
+          #  }
+          #)
+        ];
+      };
+    };
+  };
+}
