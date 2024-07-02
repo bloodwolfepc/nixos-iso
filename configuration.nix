61be32edf81f00ca9094342f9f0b64fdaeec5817
bloodwolfepc <bloodwolfepc@gmail.com>
2024-07-02 17:14:21 -0500

    commit

--- configuration.nix
+++ configuration.nix
@@ -5,7 +5,7 @@
   nixpkgs.hostPlatform = "x86_64-linux";
   nixpkgs.config.allowUnfree = true;
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
-  hardware.raid.HPSmartArray.enbale = true;
+  hardware.raid.HPSmartArray.enable = true;
   
   environment.systemPackages = with pkgs; [
     inputs.neovim.packages.x86_64-linux.default
